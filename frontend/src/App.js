33aa8c5d4470392ce0bbe159bb74561cb9f73c23
rob <robert_regan86@yahoo.com>
2022-09-30 21:28:29 -0400

    home screen products and ratings components working, need to change a tags to Link

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,22 @@
 import Header from './components/Header'
 import Footer from './components/Footer'
 import { Container } from 'react-bootstrap'
+import HomeScreen from './screens/HomeScreen'
+import ProductScreen from './screens/ProductScreen'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 const App = () => {
   return (
-    <>
+    <Router>
       <Header />
       <main className='py-3'>
         <Container>
-          <h1>Welcome to snowShop</h1>
+          <Route path='/' component={HomeScreen} exact />
+          <Route path='/product/:id' component={ProductScreen} exact />
         </Container>
       </main>
       <Footer />
-    </>
+    </Router>
   )
 }
 
