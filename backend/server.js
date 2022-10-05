0a859ac25c0071de728a8a32cc7aecb6d6edf496
rob <robert_regan86@yahoo.com>
2022-10-05 08:58:34 -0400

    user auth with email & pw

--- backend/server.js
+++ backend/server.js
@@ -5,17 +5,21 @@ const PORT = process.env.PORT || 5000
 import connectDB from './config/db.js'
 import colors from 'colors'
 import productRoutes from './routes/productRoutes.js' // with es modules, we need to use the .js ext to bring in files
+import userRoutes from './routes/userRoutes.js'
 import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 dotenv.config()
 const app = express()
 
 connectDB()
 
+app.use(express.json())
+
 app.get('/', (req, res) => {
   res.send('Api is out here spinnin..')
 })
 
 app.use('/api/products', productRoutes)
+app.use('/api/users', userRoutes)
 
 app.use(notFound)
 app.use(errorHandler)
